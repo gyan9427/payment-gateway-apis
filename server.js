b46355d70baefe9d7dbebd0fbec4059163612691
gyan3x <gyandev.m@simplify3x.com>
2023-06-05 12:26:10 +0530

    ssl certification added successfully

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const bodyParser = require("body-parser")
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+    key: fs.readFileSync(__dirname +'/ssl/privatekey.key','utf-8'),     // Path to the private key file
+    cert: fs.readFileSync(__dirname +'/ssl/certificate.crt','utf-8'),   // Path to the SSL/TLS certificate file
+  };
+
 
 app.use(bodyParser.json());
 const userRoutes = require("./routes/Users");
@@ -10,11 +18,14 @@ const gpayRoutes = require("./routes/Gpay")
 app.use("/pga/v1.0",userRoutes);
 app.use("/pga/v1.0/gpaymock",gpayRoutes);
 
-app.listen(3000,()=>{
-    mongoose.connect('mongodb://localhost:27017/paymentgateway').then(()=>{
-        console.log("connection established..")
-    }).catch(err=>{
-        console.log(err);
-    })
-    console.log("listening to 3000....")
-})
+
+https
+    .createServer(options,app)
+    .listen(3000,()=>{
+        mongoose.connect('mongodb://localhost:27017/paymentgateway').then(()=>{
+            console.log("connection established..")
+        }).catch(err=>{
+            console.log(err);
+        })
+        console.log("listening to 3000....")
+    });
